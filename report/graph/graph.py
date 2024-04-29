5510f988ceb828779a6972ee622163a4f346b825
凛ドン <384693801@qq.com>
2024-04-29 15:21:51 +0900

    result update

--- report/graph/graph.py
+++ report/graph/graph.py
@@ -4,11 +4,12 @@
 @ Author: Rindon
 @ Date: 2024-04-29 14:19:32
 @ LastEditors: Rindon
-@ LastEditTime: 2024-04-29 14:52:35
+@ LastEditTime: 2024-04-29 15:21:00
 @ Description: draw graph
 '''
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import os
 
 directory = r'D:\ISLES2018seg_BA\report\graph' 
@@ -21,14 +22,16 @@ for file in files:
     filepath = os.path.join(directory, file)
     data = pd.read_csv(filepath)
     Dice = data.iloc[:, 4]
+    #Loss = data.iloc[:, 2]
     Epoch = range(1, 301)
     plt.plot(Epoch, Dice, label=file[:-4])  # 去掉文件名的'.csv'后缀
 
 plt.legend()
-plt.title('Training dice',fontsize = 20)
+plt.title('Validation Dice',fontsize = 20)
 plt.xlabel('Epoch',fontsize = 15)
 plt.ylabel('Dice',fontsize = 15)
 plt.xlim((0,300))
 plt.ylim((0,0.72))
+plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(0.02)) 
 plt.grid()
 plt.show()
